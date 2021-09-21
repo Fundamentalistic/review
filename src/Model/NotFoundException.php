57d6c2ec898b080b953dc92459b004aafbc5c11e
Fundamentalistic <68327988+Fundamentalistic@users.noreply.github.com>
2021-09-21 16:22:38 +0700

    Comment 7

--- src/Model/NotFoundException.php
+++ src/Model/NotFoundException.php
@@ -2,6 +2,6 @@
 
 namespace App\Model;
 
-class NotFoundException extends \Exception
+class NotFoundException extends \Exception // Пустое исключение? А где переопределение методов? Зачем тогда вообще этот код? 
 {
 }
