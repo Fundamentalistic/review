7767f2ea45693252a2496e28baf61a8741843110
Fundamentalistic <68327988+Fundamentalistic@users.noreply.github.com>
2021-09-21 16:02:56 +0700

    Comment 4

--- src/Application.php
+++ src/Application.php
@@ -7,5 +7,6 @@ class Application
     public function run()
     {
         // TODO: implement routes initialization, handle request, etc
+        // Ввиду строки выше, данный код ничего не делает и делать не может.  
     }
 }
