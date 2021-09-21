16baa14c361392ed0cd527a8161492f3b8149225
Fundamentalistic <68327988+Fundamentalistic@users.noreply.github.com>
2021-09-21 15:58:14 +0700

    Comment1

--- database/schema.sql
+++ database/schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE project (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP    # Если планируется выборка по дате создания, нет соответсвующего индекса
 ) Engine=InnoDB;
 
 CREATE TABLE task (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     project_id INT NOT NULL,
     title VARCHAR(255) NOT NULL,
-    status VARCHAR(16) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    status VARCHAR(16) NOT NULL,                    # Аналогично нет индекса по статусу
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP   # Если планируется выборка по дате создания, нет соответсвующего индекса
 ) Engine=InnoDB;
