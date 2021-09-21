45a5dfbafd14469ec58d47483cc88bf74ab79501
Fundamentalistic <68327988+Fundamentalistic@users.noreply.github.com>
2021-09-21 16:16:39 +0700

    Comment 5

--- src/Storage/DataStorage.php
+++ src/Storage/DataStorage.php
@@ -14,6 +14,7 @@ class DataStorage
     public function __construct()
     {
         $this->pdo = new \PDO('mysql:dbname=task_tracker;host=127.0.0.1', 'user');
+        // Нет пароля. Нет проверки соединения. Код лучше обернуть в try - catch
     }
 
     /**
@@ -22,6 +23,7 @@ class DataStorage
      */
     public function getProjectById($projectId)
     {
+        // Нет проверки входа. Возможна передача очень большого числа. 
         $stmt = $this->pdo->query('SELECT * FROM project WHERE id = ' . (int) $projectId);
 
         if ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
@@ -40,6 +42,7 @@ class DataStorage
     {
         $stmt = $this->pdo->query("SELECT * FROM task WHERE project_id = $project_id LIMIT ?, ?");
         $stmt->execute([$limit, $offset]);
+        // Возможна SQL инъекция
 
         $tasks = [];
         foreach ($stmt->fetchAll() as $row) {
@@ -63,9 +66,10 @@ class DataStorage
             return is_string($v) ? '"' . $v . '"' : $v;
         }, $data));
 
-        $this->pdo->query("INSERT INTO task ($fields) VALUES ($values)");
-        $data['id'] = $this->pdo->query('SELECT MAX(id) FROM task')->fetchColumn();
-
+        $this->pdo->query("INSERT INTO task ($fields) VALUES ($values)"); // Возможна SQL инъекция ( опосредованно, сложно, но возможна )
+        $data['id'] = $this->pdo->query('SELECT MAX(id) FROM task')->fetchColumn(); // Тут требуется блокировка таблиц. Возможна ситуация,
+                                                                                    // при которой получение максимального айди произойдет 
+                                                                                    // после внесения других данных
         return new Model\Task($data);
     }
 }
